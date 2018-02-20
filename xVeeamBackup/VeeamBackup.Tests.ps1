0f3edadae28e7a922d1670b274b71af5b32b0db1
Feindel <gfeindel@freudenbergmedical.com>
2018-02-20 15:58:08 -0500

    - Updated Readme to reflect new project goals. - Added Pester tests. - Corrected param() syntax in VeeamBackup.psm1.

---
+++ xVeeamBackup/VeeamBackup.Tests.ps1
@@ -0,0 +1,19 @@
+﻿# To run these tests, execute Invoke-Pester in this working directory.
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+$sut = $sut -replace 'ps1','psm1'
+
+Import-Module "$here\$sut" -Force
+
+Describe "VeeamBackup" {
+    Context 'Get-TargetResource' {
+        It "Returns false if the provided job does not exist" {
+            Get-TargetResource -JobName "Bogus job" | Should be $false
+        }
+    }
+    Context "Set-TargetResource" {
+    }
+    Context "Test-TargetResource" {
+    }
+}
--- xVeeamBackup/VeeamBackup.psm1
+++ xVeeamBackup/VeeamBackup.psm1
@@ -1,68 +1,75 @@
 function Get-TargetResource {
-[ValidateNotNullOrEmpty()]
-[string]$JobName,
+    param(
+        [ValidateNotNullOrEmpty()]
+        [string]$JobName,
 
-[ValidateSet('Present','Absent')]
-[string]$Ensure,
+        [ValidateSet('Present','Absent')]
+        [string]$Ensure,
 
-[ValidateSet('Daily','Weekly','Monthly')]
-[string]$Frequency,
+        [ValidateSet('Daily','Weekly','Monthly')]
+        [string]$Frequency,
 
-[string]$BackupRepositoryName,
+        [string]$BackupRepositoryName,
 
-[ValidateSet('Incremental','ReverseIncremental')]
-[string]$BackupMode,
+        [ValidateSet('Incremental','ReverseIncremental')]
+        [string]$BackupMode,
 
-[ValidateSet('None','DedupeFriendly','Optimal','High','Extreme')]
-[string]$CompressionLevel,
+        [ValidateSet('None','DedupeFriendly','Optimal','High','Extreme')]
+        [string]$CompressionLevel,
 
-[ValidatePattern('[a-zA-Z0-9_.+-]+@[a-zA-Z0-9_.+-]\.[a-zA-Z]{2,}')]
-[string[]]$NotificationRecipients
+        [ValidatePattern('[a-zA-Z0-9_.+-]+@[a-zA-Z0-9_.+-]\.[a-zA-Z]{2,}')]
+        [string[]]$NotificationRecipients
+    )
 
+    $true
 }
 
 function Set-TargetResource {
-    [ValidateNotNullOrEmpty()]
-    [string]$JobName,
-    
-    [ValidateSet('Present','Absent')]
-    [string]$Ensure,
-    
-    [ValidateSet('Daily','Weekly','Monthly')]
-    [string]$Frequency,
-    
-    [string]$BackupRepositoryName,
-    
-    [ValidateSet('Incremental','ReverseIncremental')]
-    [string]$BackupMode,
-    
-    [ValidateSet('None','DedupeFriendly','Optimal','High','Extreme')]
-    [string]$CompressionLevel,
-    
-    [ValidatePattern('[a-zA-Z0-9_.+-]+@[a-zA-Z0-9_.+-]\.[a-zA-Z]{2,}')]
-    [string[]]$NotificationRecipients
-    
+    param(
+        [ValidateNotNullOrEmpty()]
+        [string]$JobName,
+        
+        [ValidateSet('Present','Absent')]
+        [string]$Ensure,
+        
+        [ValidateSet('Daily','Weekly','Monthly')]
+        [string]$Frequency,
+        
+        [string]$BackupRepositoryName,
+        
+        [ValidateSet('Incremental','ReverseIncremental')]
+        [string]$BackupMode,
+        
+        [ValidateSet('None','DedupeFriendly','Optimal','High','Extreme')]
+        [string]$CompressionLevel,
+        
+        [ValidatePattern('[a-zA-Z0-9_.+-]+@[a-zA-Z0-9_.+-]\.[a-zA-Z]{2,}')]
+        [string[]]$NotificationRecipients
+    )
+    $true
 }
 
 function Test-TargetResource {
-    [ValidateNotNullOrEmpty()]
-    [string]$JobName,
-    
-    [ValidateSet('Present','Absent')]
-    [string]$Ensure,
-    
-    [ValidateSet('Daily','Weekly','Monthly')]
-    [string]$Frequency,
-    
-    [string]$BackupRepositoryName,
-    
-    [ValidateSet('Incremental','ReverseIncremental')]
-    [string]$BackupMode,
-    
-    [ValidateSet('None','DedupeFriendly','Optimal','High','Extreme')]
-    [string]$CompressionLevel,
-    
-    [ValidatePattern('[a-zA-Z0-9_.+-]+@[a-zA-Z0-9_.+-]\.[a-zA-Z]{2,}')]
-    [string[]]$NotificationRecipients
-    
+    param(
+        [ValidateNotNullOrEmpty()]
+        [string]$JobName,
+        
+        [ValidateSet('Present','Absent')]
+        [string]$Ensure,
+        
+        [ValidateSet('Daily','Weekly','Monthly')]
+        [string]$Frequency,
+        
+        [string]$BackupRepositoryName,
+        
+        [ValidateSet('Incremental','ReverseIncremental')]
+        [string]$BackupMode,
+        
+        [ValidateSet('None','DedupeFriendly','Optimal','High','Extreme')]
+        [string]$CompressionLevel,
+        
+        [ValidatePattern('[a-zA-Z0-9_.+-]+@[a-zA-Z0-9_.+-]\.[a-zA-Z]{2,}')]
+        [string[]]$NotificationRecipients
+    )
+    $true
 }
